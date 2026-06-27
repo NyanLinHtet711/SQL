1437ef5256312efec02bd800f608833c7d6a9090
NyanLinHtet <u6430031@au.edu>
2026-06-28 02:17:18 +0700

    CTE examples

--- CTE.sql
+++ CTE.sql
@@ -24,3 +24,18 @@ GROUP BY gender
 )
 SELECT AVG(avg_sal),AVG(max_sal)
 FROM CTE_Example;
+
+
+
+/*
+The following is the same with above example.
+*/
+SELECT AVG(avg_sal)
+FROM
+(
+SELECT gender,AVG(salary) avg_sal,MAX(salary) max_sal,MIN(salary)min_sal,COUNT(salary) count_sal -- FOR Parks and REC create DB
+FROM employee_demographics Demo
+JOIN employee_salary Sal
+	On Demo.employee_id = Sal.employee_id
+GROUP BY gender
+)subQuary_exp;
