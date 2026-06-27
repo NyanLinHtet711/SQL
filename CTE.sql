0396ea71e58588a852525484198678a29d70875f
NyanLinHtet <u6430031@au.edu>
2026-06-28 02:36:34 +0700

    Update CTE examples in SQL

--- CTE.sql
+++ CTE.sql
@@ -25,10 +25,21 @@ GROUP BY gender
 SELECT AVG(avg_sal),AVG(max_sal)
 FROM CTE_Example;
 
+-- same example --> dr ka select mhr * tone pe apaw mhr column name twy yay tr 
+WITH CTE_Example(Gender,AVG_sal,Max_sal,Min_sal,Count_sal) AS 
+(
+SELECT gender,AVG(salary) avg_sal,MAX(salary) max_sal,MIN(salary)min_sal,COUNT(salary) count_sal -- FOR Parks and REC create DB
+FROM employee_demographics Demo
+JOIN employee_salary Sal
+	On Demo.employee_id = Sal.employee_id
+GROUP BY gender
+)
+SELECT *
+FROM CTE_Example;
 
 
 /*
-The following is the same with above example.
+The following is the same with above example. But Above one is clear version in professional area.
 */
 SELECT AVG(avg_sal)
 FROM
