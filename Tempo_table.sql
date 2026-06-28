ed7e5df7d92b32f972ff9fcad9a32ef95ebaefe0
NyanLinHtet <u6430031@au.edu>
2026-06-28 13:23:10 +0700

    TEMPORARY TABLE in sql

---
+++ Tempo_table.sql
@@ -0,0 +1,22 @@
+CREATE TEMPORARY TABLE temp_table
+(
+first_name varchar(50),
+last_name varchar(50),
+fav_movie varchar(100)
+);
+
+SELECT *
+FROM temp_table;
+
+INSERT INTO temp_table
+VALUES(
+'Alex','Fugen','Friends'
+);
+
+CREATE TEMPORARY TABLE sal_over_50k
+SELECT *
+FROM employee_salary
+WHERE salary>50000;
+
+SELECT *
+FROM sal_over_50k;
